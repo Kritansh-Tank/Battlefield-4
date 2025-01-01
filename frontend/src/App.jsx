3497c83f81823523b7d149b7b7192a4798a2ffe5
Kritansh Tank <tankkritansh088@gmail.com>
2025-01-01 22:06:44 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,13 +24,18 @@ function App() {
     let metaDescription = "";
 
     switch (pathname) {
+      case "/":
+        title = "Welcome to Battlefield 4 - Server Info";
+        metaDescription =
+          "Explore Battlefield 4 multiplayer server information.";
+        break;
       case "/menu multiplayer/server browser/server info":
-        title = "";
-        metaDescription = "";
+        title = "Server Info - Battlefield 4";
+        metaDescription = "Detailed server information for Battlefield 4.";
         break;
       case "/menu multiplayer/server browser/server info/show more":
-        title = "";
-        metaDescription = "";
+        title = "More Server Info - Battlefield 4";
+        metaDescription = "Additional server details for Battlefield 4.";
         break;
     }
 
@@ -50,7 +55,14 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/menu multiplayer/server browser/server info" element={<MenuMultiplayerServerBrow />} />
+      <Route
+        path="/"
+        element={<h1>Welcome to Battlefield 4 - Server Info</h1>}
+      />
+      <Route
+        path="/menu multiplayer/server browser/server info"
+        element={<MenuMultiplayerServerBrow />}
+      />
       <Route
         path="/menu multiplayer/server browser/server info/show more"
         element={<MenuMultiplayerServerBrow1 />}
@@ -58,4 +70,5 @@ function App() {
     </Routes>
   );
 }
+
 export default App;
